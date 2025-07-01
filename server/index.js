fc4cedd460483cb7f14ea3a7b006ed6e4d6ac129
Abhishek Verma <neeleshabhishek@gmail.com>
2025-07-01 11:46:26 +0530

    changes

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+
+//  setup cors
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log("Connected to mongo db")).catch((err) => console.error("Mongo connection falied with error: ", err));
