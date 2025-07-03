73e4b357b3c827953d4a7df878bb022cae332817
Abhishek Verma <neeleshabhishek@gmail.com>
2025-07-03 22:06:11 +0530

    Middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,1 +1,39 @@
-//  this is to check whether     user is logged in by checking JWT token  in short it is authentication middleware
+//  this is to check whether     user is logged in by checking JWT token
+//  this middleware  verifies jwt token ensure user exsist
+//  ensure protected routes  if authentiction succeeds
+
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorisation")?.replace("Bearer", "");
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "No authentication tokenm, access denied" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    //  finding the user and verifying if user exist
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(401).json({ message: "User not found " });
+    }
+
+    //  attachin the user information to the  request
+
+    req.user = {
+      userId: user._id,
+      username: user.username,
+    };
+    next();
+  } catch (error) {
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ message: "Token has expired" });
+    }
+    res.status(401).json({ message: "Authentication failed" });
+  }
+};
+
+module.exports = auth;
