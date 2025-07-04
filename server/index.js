7c32f71d244dea58b3b73310f1ff51e616b1747b
Abhishek Verma <neeleshabhishek@gmail.com>
2025-07-04 11:40:18 +0530

    Fix

--- server/index.js
+++ server/index.js
@@ -7,15 +7,32 @@ dotenv.config();
 
 const app = express();
 
-
 //  setup cors
 app.use(cors());
 app.use(express.json());
 
 //   connecting to the mongo db
 
-mongoose.connect(process.env.MONGODB_URL).then(() => console.log("Connected to mongo db")).catch((err) => console.error("Mongo connection falied with error: ", err));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to mongo db"))
+  .catch((err) => console.error("Mongo connection falied with error: ", err));
 
+//  imorting routes
 
+const authRoutes = require("./routes/auth");
 
+//  use routes
 
+app.use("/api/auth", authRoutes);
+
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+// port decided by hosting platform
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
+});
+const PORT = process.env.PORT;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtokn");
-
+const jwt = require("jsonwebtoken");
+const auth=require("../middleware/auth")
 //  registration of user
 router.post("/register", async (req, res) => {
   try {
