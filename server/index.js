6466c2d6afa8582c84a8eaf36732022b7a149204
Abhishek Verma <neeleshabhishek@gmail.com>
2025-07-06 22:38:30 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname1, "../client/dist/index.html"));
 });
 
+
 const PORT = process.env.PORT;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
