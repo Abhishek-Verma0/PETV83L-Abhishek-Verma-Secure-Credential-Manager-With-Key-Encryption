28be31d55edc6851a97b89a0a4b425b7627c48a3
Abhishek-Verma0 <neeleshabhishek@gmail.com>
2025-07-08 15:49:36 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig,loadEnv, preview } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, preview } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  
   const env = loadEnv(mode, process.cwd(), "");
 
   const config = {
     plugins: [react()],
     server: {
       host: true,
-      port: process.env.VITE_PORT,
+      port: process.env.VITE_PORT || 5173,
       strictPort: false,
-
       proxy: {
         "/api": {
           target: env.VITE_API_URL,
@@ -23,9 +21,20 @@ export default defineConfig(({ mode }) => {
     },
     preview: {
       host: true,
-      port: process.env.VITE_PORT,
+      port: process.env.VITE_PORT || 5173,
       strictPort: false,
+    },
+    build: {
+      outDir: "dist",
+      target: "esnext",
+      minify: false,
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
+      },
     },
   };
   return config;
-})
+});
